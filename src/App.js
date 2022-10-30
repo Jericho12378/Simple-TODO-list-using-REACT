4c58c7ec231a40963b1cee5fd43031f1b8941811
Jericho Ferriol <jericho12378@gmail.com>
2022-10-30 21:43:53 +0800

    list can now be deleted

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 import { useState } from "react";
 import "./App.css";
 import InputField from "./components/inputField.jsx";
@@ -8,35 +10,66 @@ function App() {
   let counter = localStorage.length + 1;
   let todoLists = [];
 
-  for (let i = 1; i <= localStorage.length; i++) {
-    let string = JSON.parse(localStorage.getItem("todo" + i))
-    todoLists.push(string);
+  // for (let i = 1; i <= localStorage.length; i++) {
+  //   console.log(localStorage.key(i-1))
+  //   let string
+  //   let x = 0
+  //  // console.log(i)
+  //   if(localStorage.getItem("todo" + i) == null ){
+  //   // console.log(i + " is null")
+  //     x = i + 1  
+  //     string = JSON.parse(localStorage.getItem("todo" + x))
+  //   }else{
+  //     string = JSON.parse(localStorage.getItem("todo" + i))
+      
+  //     todoLists.push(string); 
+  //    // console.log(todoLists)
+  //   }
+      
+  // }
+  setLocalStorage()
+  function setLocalStorage(){
+    for (let i = 0; i < localStorage.length; i++) {
+      let localKey = localStorage.key(i)
+      let string = JSON.parse(localStorage.getItem(localKey))
+      todoLists.push(string); 
+    }
   }
   
+
   return (
     <div className="App">
       <div className="App__header">
-        <h1>My Todo Lists</h1>
+        <h1>My Todo Lists </h1>
       </div>
       <div className="todo__wrapper">
         <div className="input__wrapper">
           <InputField ButtonClicked={ButtonClicked}></InputField>
         </div>
         <div className="lists__wrapper">
-          {todoLists.map(lists => (
-              <ListsOfTodo key={lists} lists={lists}></ListsOfTodo>
+          {todoLists.map((lists,index) => (
+              <ListsOfTodo key={lists} lists={lists}
+               deleteButton={() => deleteButton(lists,index)}></ListsOfTodo>
+              
              ))}
         </div>
       </div>
     </div>
   );
 
+  function deleteButton(value,index){
+    let localKey = localStorage.key(index)
+    localStorage.removeItem(localKey)
+    setLocalStorage()
+    setTodo([value]) //The purpose of having this is to refresh the damn component only!! its like pressing f5 while youre on the desktop (just so you know future me)
+  }
+
   function ButtonClicked() {
     let inputValue = document.getElementById("input").value;
     if (counter == null) {
       counter = 1;
     }
-    setTodo([inputValue])
+    setTodo([inputValue]) //Another one here HAHHAHA USING USESTATE OF REACT IN  A VERY ODD WAY AHAHHAHAHAHA IM TALKING TO MYSELF
     localStorage.setItem(`todo${counter}`, JSON.stringify(inputValue));
     // counter = localStorage.key(localStorage.length)
     counter++;
--- src/components/listOfTodos.jsx
+++ src/components/listOfTodos.jsx
@@ -1,9 +1,12 @@
-function lists({ lists }) {
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+function lists({ lists, deleteButton }) {
   return (
     <div className="lists">
         <div className="list">
           <input type="checkbox" name="" id="checkbox" />
           <h4 className="todo">{lists}</h4>
+          <FontAwesomeIcon onClick={deleteButton} className="trashCan" icon={ faTrash }/>
         </div>
     </div>
   );
